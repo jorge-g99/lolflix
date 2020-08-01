51641b45bd7e7e332eb1e00fb3ddbe3a5b6c472e
jorge-g99 <44658260+jorge-g99@users.noreply.github.com>
2020-08-01 13:10:57 -0300

    update4.2

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -29,7 +29,9 @@ function CadastroCategoria() {
 
   useEffect(() => {
     if (window.location.href.includes('localhost')) {
-      const URL = 'http://localhost:8080/categorias';
+      const URL = window.location.hostname.includes('localhost') 
+        ? 'http://localhost:8080/categorias'
+        : 'https://lolflixx.herokuapp.com/categorias';
       fetch(URL)
         .then(async (respostaDoServer) => {
           if (respostaDoServer.ok) {
